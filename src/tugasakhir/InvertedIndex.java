269bbefe0618055b27121370ce0670f36c0bd9a6
Unknown <elizabeth.winnie.kurniawan@gmail.com>
2019-11-08 17:39:19 +0700

    Inverted Index (write-load)

--- src/tugasakhir/InvertedIndex.java
+++ src/tugasakhir/InvertedIndex.java
@@ -5,10 +5,145 @@
  */
 package tugasakhir;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 /**
  *
  * @author reggi
  */
 public class InvertedIndex {
-    
+
+    private static Map<String, List> invertedIndex = new TreeMap<String, List>();
+
+    public InvertedIndex() {
+    }
+
+    public static void createInvertedIndex(File[] filesCleaned) throws FileNotFoundException {
+        for (int i = 0; i < filesCleaned.length; i++) {
+            Scanner input = new Scanner(filesCleaned[i]);
+            String namaDoc = filesCleaned[i].getName();
+            int titik = namaDoc.indexOf(".");
+            namaDoc = namaDoc.substring(0, titik);
+            while (input.hasNext()) {
+                String word = input.next();
+                if (!word.equalsIgnoreCase("'")) {
+                    List<String> valueWord = invertedIndex.get(word);
+                    //get dari map apakah word sudah ada atau belum jika sudah ada, maka tambahkan, jika tidak, maka buat map baru
+                    if (valueWord != null) {
+                        if (!valueWord.contains(namaDoc)) {
+                            valueWord.add(namaDoc);
+                            invertedIndex.put(word, valueWord);
+                        }
+                    } else {
+                        ArrayList<String> postingList = new ArrayList<String>();
+                        postingList.add(namaDoc);
+                        invertedIndex.put(word, postingList);
+                    }
+                }
+            }
+        }
+    }
+
+    public void saveMaps() {
+        String path = System.getProperty("user.dir") + "\\inv_idx\\inv_idx.list";
+
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
+            oos.writeObject(this.getInvIndex());
+            oos.close();
+        } catch (Exception e) {
+            // Catch exceptions
+        }
+    }
+
+    public void loadMaps() {
+        String path = System.getProperty("user.dir") + "\\inv_idx\\inv_idx.list";
+        Map<String, List> invIndexLoad = new TreeMap<String, List>();
+
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
+            Object readMap = ois.readObject();
+            if (readMap != null && readMap instanceof TreeMap) {
+                invIndexLoad.putAll((TreeMap) readMap);
+            }
+            ois.close();
+        } catch (Exception e) {
+            // Catch exceptions
+        }
+
+        for (Map.Entry entry : invIndexLoad.entrySet()) {
+//            invIndexLoad.put(entry.getKey().toString(), (List)entry.getValue());
+            System.out.println("key: " + entry.getKey() + "; value: " + entry.getValue());
+        }
+    }
+
+//    public void savingMapIntoFile() throws FileNotFoundException, IOException {
+//        Properties properties = new Properties();
+//        String path = System.getProperty("user.dir") + "\\inv_idx\\";
+//        File file = new File(path);
+//        file.mkdirs();
+//        PrintWriter writer = new PrintWriter(path + "invertedIndex.txt");
+//        for (Map.Entry<String, List> entry : invertedIndex.entrySet()) {
+//            writer.println(entry.getKey() + " " + entry.getValue());
+//            properties.put(entry.getKey(), entry.getValue());
+//            System.out.println(entry.getKey() + " => " + entry.getValue());
+//        }
+//        properties.store(new FileOutputStream("invertedIndex.properties"), null);
+//        writer.close();
+//    }
+
+//    public static Map<String, ArrayList> loadMapFromFile() throws FileNotFoundException, IOException {
+//        Map<String, ArrayList> invIndex = new HashMap<String, ArrayList>();
+//        Properties properties = new Properties();
+//        properties.load(new FileInputStream("invertedIndex.properties"));
+
+//        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("invertedIndex.properties"));
+//        out.writeObject(invIndex);
+//        out.close();
+//
+//        for (String key : properties.stringPropertyNames()) {
+//            System.out.println(key + " => " + properties.get(key).getClass());
+////            invIndex.put(key, (ArrayList)properties.get(key));
+//        }
+//        return invIndex;
+//    }
+
+    public Map<String, List> getInvIndex() {
+        return this.invertedIndex;
+    }
+
+//    public static void main(String[] args) throws IOException {
+//        InvertedIndex invIndex = new InvertedIndex();
+//        createInvertedIndex(findFilesInDirectory(System.getProperty("user.dir") + "\\cleaned_dataset\\"));
+//        invIndex.saveMaps();
+//        invIndex.loadMaps();
+
+//        Map<String, ArrayList> hasil = loadMapFromFile();
+//
+//        for (Map.Entry<String, ArrayList> entry : hasil.entrySet()) {
+////            writer.println(entry.getKey() + ", " + entry.getValue());
+////            properties.put(entry.getKey(), entry.getValue());
+//            System.out.println(entry.getKey() + " => " + entry.getValue());
+//        }
+//    }
+
+    public static File[] findFilesInDirectory(String directoryPath) {
+        File folder = new File(directoryPath);
+        File[] listOfFiles = folder.listFiles();
+        System.out.println("Jumlah documents : " + listOfFiles.length);
+        return listOfFiles;
+    }
 }
+
+
--- src/tugasakhir/Main.java
+++ src/tugasakhir/Main.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugasakhir;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  *
@@ -11,7 +9,27 @@ package tugasakhir;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        String path_idx = System.getProperty("user.dir") + "\\inv_idx\\";
+        File folder_idx = new File(path_idx);
+        File[] contents = folder_idx.listFiles();
+        if (contents.length == 0) {
+            System.out.println("false"); //jika inv idx belum ada
+            InvertedIndex invIndex = new InvertedIndex();
+            invIndex.createInvertedIndex(findFilesInDirectory(System.getProperty("user.dir") + "\\cleaned_dataset\\"));
+            invIndex.saveMaps();
+            invIndex.loadMaps();
+        } else {
+            System.out.println("true"); //jika inv idx sudah ada
+        }
+    }
+
+    //    method untuk membaca file document yang ada di sebuah folder
+    public static File[] findFilesInDirectory(String directoryPath) {
+        File folder = new File(directoryPath);
+        File[] listOfFiles = folder.listFiles();
+        System.out.println("Jumlah documents : " + listOfFiles.length);
+        return listOfFiles;
     }
 }
+
