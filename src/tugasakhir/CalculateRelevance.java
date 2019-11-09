b81bdda04e8b5132e3ecc823ad0dfddb4ecadee3
Reg <reggiegunawan88@gmail.com>
2019-11-09 22:33:41 +0700

    Benerin rumus Precision Recall

--- src/tugasakhir/CalculateRelevance.java
+++ src/tugasakhir/CalculateRelevance.java
@@ -10,28 +10,23 @@ package tugasakhir;
  * @author reggi
  */
 public class CalculateRelevance {
-    private double precision, recall, f1;
-    private int x,y,z;
+    private double precision, recall;
+    private int TP,FP,FN;
     
-    //@param:
-    //x : relevant items retrieved
-    //y : retrieved items
-    //z : relevant items
-    public CalculateRelevance(int x, int y, int z){
-        this.x=x;
-        this.y=y;
-        this.z=z;
+    public CalculateRelevance(int TP, int FP, int FN){
+        this.TP=TP;
+        this.FP=FP;
+        this.FN=FN;
         this.precision = 0;
         this.recall = 0;
-        this.f1 = 0;
     }
     
     public void calc_Precision(){
-        this.precision = x/y;
+        this.precision = TP/(TP+FP);
     }
     
     public void calc_Recall(){
-        this.recall = x/z;
+        this.recall = TP/(TP+FN);
     }
     
     public double getPrecision(){
@@ -44,6 +39,6 @@ public class CalculateRelevance {
     
     //method ini dipanggil setelah calcPrecision dan calcRecall dipanggil
     public double findF1(){
-        return (2*this.getPrecision()*this.getRecall()) / this.getPrecision() + this.getRecall();
+        return (2*this.getPrecision()*this.getRecall()) / (this.getPrecision() + this.getRecall());
     }
 }
