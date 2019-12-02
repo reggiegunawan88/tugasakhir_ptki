c2c8ae67e8abddee33d21fedf8c46f25f22cf5e6
unknown <elizabeth.winnie.kurniawan@gmail.com>
2019-12-02 12:12:25 +0700

    BM25 done

--- src/tugasakhir/BM25.java
+++ src/tugasakhir/BM25.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import static tugasakhir.LanguageModel.getResult;
 
 /**
  *
@@ -40,6 +41,7 @@ public class BM25 {
         this.Lave = 0;
         this.words = words;
         this.invertedIndex = invertedIndex;
+        this.ld = new HashMap<String, Integer> ();
     }
     
     public void getLd () throws IOException
@@ -64,22 +66,28 @@ public class BM25 {
         this.Lave = this.jumlahWordSemuaDoc*1.0/ld.size()*1.0;
     }
     
-    public Map<String, Double> getBM25 ()
+    public Map<String, Double> getBM25 () throws IOException
     {
+        getLd();
         Map<String, Double> res = new TreeMap<String, Double>();
         Map<String, Integer> df = df(words, namaDoc);
         
         for (int i = 0; i < namaDoc.size(); i++) {
+            String namaDocNow = (String)namaDoc.get(i);
             double hasilDoc = 0;
             for (int j = 0; j < words.size(); j++) {
-                double hasil = Math.log10(n/);
+                String word = (String)words.get(j);
+                int dft = df.get(word);
+                double tftd = 0;
+                if (this.invertedIndex.get(word).get(namaDocNow) != null)
+                {
+                    tftd = this.invertedIndex.get(word).get(namaDocNow);
+                } 
+                double hasil = Math.log10(n/dft) * (((k1+1)*tftd)/((k1 * ((1-b)+b *(ld.get(namaDocNow)/Lave)))+(tftd*1.0)));
+                hasilDoc += hasil;
             }
-        }
-        
-//        for (Map.Entry entry : invIndexLoad.entrySet()) {
-//            System.out.println("key: " + entry.getKey() + "; value: " + entry.getValue());
-//        }
-        
+            res.put(namaDocNow, hasilDoc);
+        }        
         return res;
     }
     
@@ -87,11 +95,11 @@ public class BM25 {
     {
         Map<String, Integer> df = new HashMap<String, Integer> ();
         for (int i = 0; i < wordQuery.size(); i++) {
-            
             String word = wordQuery.get(i);
+            int jumlahDF = 0;
             Map<String, Integer> tfDoc = this.invertedIndex.get(word);
-            for (int i = 0; i < namaDoc.size(); i++) {
-                String namaDocNow = namaDoc.get(i);
+            for (int j = 0; j < namaDoc.size(); j++) {
+                String namaDocNow = namaDoc.get(j);
                 if (tfDoc.containsKey(namaDocNow)) {
                     jumlahDF += tfDoc.get(namaDocNow);
                 }
@@ -100,7 +108,42 @@ public class BM25 {
         }
         return df;
     }
+    
+    public static void main (String[] args) throws IOException
+    {
+        
+        Map<String, Map<String, Integer>> invertedIndex = new TreeMap<String, Map<String, Integer>>();
+        InvertedIndex invIndex = new InvertedIndex();
+        invIndex.loadMaps();
+        invertedIndex = invIndex.getInvIndex();
+
+        ArrayList namaDoc = new ArrayList();
+        namaDoc.add("Doc001");
+        namaDoc.add("Doc005");
+        namaDoc.add("Doc016");
+        namaDoc.add("Doc021");
+        namaDoc.add("Doc065");
+        namaDoc.add("Doc068");
+        namaDoc.add("Doc069");
+        namaDoc.add("Doc098");
+        namaDoc.add("Doc099");
+        namaDoc.add("Doc124");
+
+        ArrayList words = new ArrayList();
+        words.add("flower");
+        words.add("famin");
+        words.add("barren");
+        
+        BM25 bm = new BM25(namaDoc, words, invertedIndex);
+        Map<String, Double> res = bm.getBM25();
+        
+        for (Map.Entry entry : res.entrySet()) {
+            System.out.println("key: " + entry.getKey() + "; value: " + entry.getValue());
+        }
+    }
 }
+
+
 
 
 
